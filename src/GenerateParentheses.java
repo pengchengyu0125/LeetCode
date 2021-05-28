e64fe069bf18f66cc41c96d369bd447d68465515
pengchengyu0125 <767026826@qq.com>
2021-05-28 14:14:16 +0800

    generateP

---
+++ src/GenerateParentheses.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ */
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        int n = 3;
+        GenerateParentheses g = new GenerateParentheses();
+        System.out.println(g.generateParenthesis(n).toString());
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        helper(n, n, "", res);
+        return res;
+    }
+    void helper(int left, int right, String out, List<String> res) {
+        if (left < 0 || right < 0 || left > right) return;
+        if (left == 0 && right == 0) {
+            res.add(out);
+            return;
+        }
+        helper(left - 1, right, out + "(", res);
+        helper(left, right - 1, out + ")", res);
+    }
+}
